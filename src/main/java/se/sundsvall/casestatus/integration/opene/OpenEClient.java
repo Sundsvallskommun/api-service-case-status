e76b4d7b65be9f037d9715494b49ce4b61b8d721
lsjolinder <linus.sjolinder@b3.se>
2023-04-04 14:06:31 +0200

    Instead of trying to find every errand from 2021-01-01 only find since yesterday

--- src/main/java/se/sundsvall/casestatus/integration/opene/OpenEClient.java
+++ src/main/java/se/sundsvall/casestatus/integration/opene/OpenEClient.java
@@ -12,6 +12,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Component
@@ -65,7 +66,7 @@ public class OpenEClient {
                 .host(properties.getBaseUrl())
                 .port(properties.getPort())
                 .path(path)
-                .queryParam("fromDate", "2021-01-01")
+                .queryParam("fromDate", LocalDate.now().minusDays(1).toString())
                 .build(parameters);
     }
 
