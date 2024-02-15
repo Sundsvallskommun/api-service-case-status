84e8b3daa57f10dfca56923fecd3e61cfa4f61d8
AndreSoderlund <andre.soderlund@b3.se>
2024-02-15 16:30:29 +0100

    Added flyway script to add a new casedata status mapping to oep

---
+++ src/main/resources/db/migration/V3.2__add_new_status.sql
@@ -0,0 +1,5 @@
+INSERT INTO CaseManagementStatus(ID, caseManagementID, caseManagementStatus, creationDate, updateDate)
+VALUES (28, 'Under granskning', 'Under granskning', '2024-02-15 16:18:00', '2024-02-15 16:18:00');
+
+INSERT INTO RefCaseManagementOpenE (ID, caseManagementPK, openePK, creationDate, updateDate)
+VALUES (28, 28, 5, '2024-02-15 16:18:00', '2024-02-15 16:18:00');
