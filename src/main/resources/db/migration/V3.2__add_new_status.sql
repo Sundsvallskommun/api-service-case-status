ea4c003db16fb5054ac2de1df93a158d476ac560
AndreSoderlund <andre.soderlund@b3.se>
2024-02-16 10:10:05 +0100

    spelling error

--- src/main/resources/db/migration/V3.2__add_new_status.sql
+++ src/main/resources/db/migration/V3.2__add_new_status.sql
@@ -1,6 +1,6 @@
 INSERT INTO CaseManagementStatus(ID, caseManagementID, caseManagementStatus, creationDate, updateDate)
 VALUES (28, 'Under granskning', 'Under granskning', '2024-02-15 16:18:01', '2024-02-15 16:18:02'),
-       (29, 'Ärendet avslutat', 'Ärendet avslutat', '2024-02-15 16:18:03', '2024-02-15 16:18:04');
+       (29, 'Ärende avslutat', 'Ärendet avslutat', '2024-02-15 16:18:03', '2024-02-15 16:18:04');
 
 INSERT INTO RefCaseManagementOpenE (ID, caseManagementPK, openePK, creationDate, updateDate)
 VALUES (28, 28, 5, '2024-02-15 16:18:05', '2024-02-15 16:18:06'),
