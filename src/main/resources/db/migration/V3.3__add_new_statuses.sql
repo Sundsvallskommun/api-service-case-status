1961093a884fce255445db7da78fb7b88cd3a8cd
Michael Wallner <Michael.Wallner@Sundsvall.se>
2024-03-05 15:05:52 +0100

    UF-7974: Added new statuses for MEX

---
+++ src/main/resources/db/migration/V3.3__add_new_statuses.sql
@@ -0,0 +1,11 @@
+INSERT INTO CaseManagementStatus(ID, caseManagementID, caseManagementStatus, creationDate, updateDate)
+VALUES (32, 'Intern komplettering', 'Intern komplettering', '2024-03-05 16:18:03', '2024-03-05 16:18:04'),
+       (33, 'Under remiss', 'Under remiss', '2024-03-05 16:18:03', '2024-03-05 16:18:04'),
+       (34, 'Återkoppling remiss', 'Återkoppling remiss', '2024-03-05 16:18:03', '2024-03-05 16:18:04'),
+       (35, 'Intern återkoppling', 'Intern återkoppling', '2024-03-05 16:18:03', '2024-03-05 16:18:04');
+
+INSERT INTO RefCaseManagementOpenE (ID, caseManagementPK, openePK, creationDate, updateDate)
+VALUES (32, 32, 5, '2024-03-05 16:18:07', '2024-03-05 16:18:08'),
+       (33, 33, 5, '2024-03-05 16:18:07', '2024-03-05 16:18:08'),
+       (34, 34, 5, '2024-03-05 16:18:07', '2024-03-05 16:18:08'),
+       (35, 35, 5, '2024-03-05 16:18:07', '2024-03-05 16:18:08');
