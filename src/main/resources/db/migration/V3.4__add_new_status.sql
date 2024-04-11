0af298e17a3b7d6701a259196a734661d0be9240
Michael Wallner <Michael.Wallner@Sundsvall.se>
2024-04-11 12:25:44 +0200

    UF-6427: Added new status for appeal

---
+++ src/main/resources/db/migration/V3.4__add_new_status.sql
@@ -0,0 +1,8 @@
+INSERT INTO CaseManagementStatus(ID, caseManagementID, caseManagementStatus, creationDate, updateDate)
+VALUES (36, 'Under överklagan', 'Under överklagan', '2024-04-11 16:18:01', '2024-04-11 16:18:02');
+
+INSERT INTO OpenEStatus (ID, openeID, openeStatus, creationDate, updateDate)
+VALUES (9, 'Under överklagan', 'Under överklagan', '2024-04-11 16:18:01', '2024-04-11 16:18:01');
+
+INSERT INTO RefCaseManagementOpenE (ID, caseManagementPK, openePK, creationDate, updateDate)
+VALUES (36, 36, 9, '2024-04-11 16:18:05', '2024-04-11 16:18:06');
