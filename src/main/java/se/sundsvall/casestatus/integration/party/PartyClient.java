b7e0e359d3580564cc3d1116055b5d301e598de6
cheezi0747 <linus.sjolinder@b3.se>
2024-12-18 09:42:55 +0100

    UF-11355: add Circuitbreaker for party integration

--- src/main/java/se/sundsvall/casestatus/integration/party/PartyClient.java
+++ src/main/java/se/sundsvall/casestatus/integration/party/PartyClient.java
@@ -3,6 +3,7 @@ package se.sundsvall.casestatus.integration.party;
 import static se.sundsvall.casestatus.integration.party.configuration.PartyConfiguration.CLIENT_ID;
 
 import generated.se.sundsvall.party.PartyType;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.Optional;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import se.sundsvall.casestatus.integration.party.configuration.PartyConfiguratio
 	url = "${integration.party.base-url}",
 	configuration = PartyConfiguration.class,
 	dismiss404 = true)
+@CircuitBreaker(name = CLIENT_ID)
 public interface PartyClient {
 
 	@GetMapping("/{municipalityId}/{type}/{partyId}/legalId")
