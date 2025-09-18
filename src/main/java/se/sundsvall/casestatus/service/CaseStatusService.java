04c7a423224f2ce37779b32ceba1cab42cbe0398
Michael Wallner <Michael.Wallner@sundsvall.se>
2025-09-18 10:37:57 +0200

    UF-17232: Changes after review

--- src/main/java/se/sundsvall/casestatus/service/CaseStatusService.java
+++ src/main/java/se/sundsvall/casestatus/service/CaseStatusService.java
@@ -4,7 +4,7 @@ import static generated.se.sundsvall.party.PartyType.ENTERPRISE;
 import static generated.se.sundsvall.party.PartyType.PRIVATE;
 import static java.util.Collections.emptyList;
 import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.springframework.util.StringUtils.hasText;
 import static org.zalando.problem.Status.BAD_REQUEST;
 import static se.sundsvall.casestatus.service.mapper.OpenEMapper.toCasePdfResponse;
 import static se.sundsvall.casestatus.service.mapper.OpenEMapper.toOepStatusResponse;
@@ -24,7 +24,6 @@ import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
@@ -201,14 +200,14 @@ public class CaseStatusService {
 	}
 
 	public List<CaseStatusResponse> getErrandStatuses(final String municipalityId, final String propertyDesignation, final String errandNumber) {
-		if (isNotBlank(propertyDesignation) && isNotBlank(errandNumber)) {
+		if (hasText(propertyDesignation) && hasText(errandNumber)) {
 			throw Problem.valueOf(BAD_REQUEST, "Both propertyDesignation and errandNumber cannot be provided at the same time");
 		}
 		if (propertyDesignation == null && errandNumber == null) {
 			throw Problem.valueOf(BAD_REQUEST, "Either propertyDesignation or errandNumber must be provided");
 		}
 
-		if (isNotBlank(propertyDesignation)) {
+		if (hasText(propertyDesignation)) {
 			return caseDataIntegration.getNamespaces().stream()
 				.flatMap(namespace -> caseDataIntegration.getCaseDataCaseByPropertyDesignation(municipalityId, namespace, propertyDesignation).stream())
 				.toList();
@@ -296,7 +295,7 @@ public class CaseStatusService {
 		return statusesRepository.findByOepStatus(oepStatus.getName()).stream()
 			.map(StatusesEntity::getExternalStatus)
 			.filter(Objects::nonNull)
-			.filter(StringUtils::isNotBlank)
+			.filter(org.springframework.util.StringUtils::hasText)
 			.findFirst()
 			.orElse(DEFAULT_EXTERNAL_STATUS);
 	}
@@ -305,7 +304,7 @@ public class CaseStatusService {
 
 		return statusResponses.stream()
 			.map(response -> {
-				if (isNotBlank(response.getStatus())) {
+				if (hasText(response.getStatus())) {
 					final var externalStatus = statusesRepository.findByCaseManagementStatus(response.getStatus())
 						.map(StatusesEntity::getExternalStatus)
 						.orElse(DEFAULT_EXTERNAL_STATUS);
@@ -317,11 +316,11 @@ public class CaseStatusService {
 
 	private CaseStatusResponse addExternalStatusByOepStatus(CaseStatusResponse caseStatusResponse) {
 
-		if (isNotBlank(caseStatusResponse.getStatus())) {
+		if (hasText(caseStatusResponse.getStatus())) {
 			final var externalStatus = statusesRepository.findByOepStatus(caseStatusResponse.getStatus()).stream()
 				.map(StatusesEntity::getExternalStatus)
 				.filter(Objects::nonNull)
-				.filter(StringUtils::isNotBlank)
+				.filter(org.springframework.util.StringUtils::hasText)
 				.findFirst()
 				.orElse(DEFAULT_EXTERNAL_STATUS);
 			caseStatusResponse.setExternalStatus(externalStatus);
